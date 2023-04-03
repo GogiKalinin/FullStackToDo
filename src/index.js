a5a6ff3142ebb8f9988abe695081fce214c2874e
GogiKalinin <bbdostoevskij@gmail.com>
2023-04-03 17:38:16 +0300

    added redux and react-redux modules

--- src/index.js
+++ src/index.js
@@ -3,13 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStoreHook } from 'react-redux';
+import { catchClause } from '@babel/types';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const defaultState = {
+  cash: 0
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload }
+      
+    case "GET_CASH":
+      return {...state, cash: state.cash - action.payload }
+
+    default: 
+      return state
+  }
+}
+
+const store = createStoreHook(reducer)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>,
+document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
